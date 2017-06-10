e604a221f45862973197989b4bd13a6e71f966e6
Dave Harms <daveharmswebdev@gmail.com>
2017-06-10 12:11:05 -0500

    added /events/:id route and beginning 404 component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventDetailsComponent } from './events/event-detail/event-details.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
 
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
 
+import { appRoutes } from './routes';
+
 @NgModule({
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   declarations: [
       EventsAppComponent,
       EventsListComponent,
       EventThumbnailComponent,
       EventDetailsComponent,
-      NavBarComponent
+      NavBarComponent,
+      CreateEventComponent,
+      Error404Component
     ],
   providers: [
     EventService,
