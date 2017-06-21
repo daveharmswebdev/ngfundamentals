8f9fe6f035e52b0b46ea59578d051d427cbd6e68
Dave Harms <daveharmswebdev@gmail.com>
2017-06-20 19:23:27 -0500

    added sorting by name and vote

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,6 +1,16 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared/index';
 
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) return 1
+  else if (s1.name === s2.name) return 0
+  else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length
+}
+
 @Component({
   selector: 'session-list',
   templateUrl: 'app/events/event-details/session-list.component.html'
@@ -8,14 +18,18 @@ import { ISession } from '../shared/index';
 export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
   @Input() filterBy: string;
+  @Input() sortBy: string;
   visibleSessions: ISession[] = [];
 
   ngOnChanges() {
     if(this.sessions) {
-      console.log('change', this.filterBy)
       this.filterSessions(this.filterBy);
+      this.sortBy === 'name'
+        ? this.visibleSessions.sort(sortByNameAsc)
+        : this.visibleSessions.sort(sortByVotesDesc)
     }
   }
+
 
   filterSessions(filter) {
     console.log('filter');
